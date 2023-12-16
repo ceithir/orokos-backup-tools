0ac76620d947d1360aacac6cbc791ed88ec267aa
Ceithir <ceathramh@gmail.com>
2023-12-16 13:50:07 +0100

    Script to scrap all campaigns from Orokos
    Under their url friendly name

---
+++ import-campaigns.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+
+import urllib.request
+from bs4 import BeautifulSoup
+
+def import_campaigns(page = 1):
+    url = 'https://orokos.com/roll/?action=view_all&offset='+str((page-1)*20)
+    raw_html = urllib.request.urlopen(url).read()
+    soup = BeautifulSoup(raw_html, 'html.parser')
+    campaigns = set(map(lambda elt: elt['href'], soup.css.select('a[href^="c-"]')))
+    return campaigns
+
+if __name__ == "__main__":
+    campaigns = set()
+
+    for i in range(1, 50000):
+        new_campaigns = import_campaigns(i+1)
+        if not new_campaigns:
+            break
+        campaigns = campaigns.union(new_campaigns)
+
+    with open('campaigns.txt', 'w') as f:
+        for line in sorted(campaigns):
+            f.write(f"{line}\n")
